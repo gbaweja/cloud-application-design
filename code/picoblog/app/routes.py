8b9432e6f9aa9e6824eec5c4288043b62b32c1ec
Greg DeLozier <gregdelozier@gmail.com>
2018-05-02 22:03:27 -0400

    Final Update

--- code/picoblog/app/routes.py
+++ code/picoblog/app/routes.py
@@ -4,5 +4,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Everyone'}
+    user = {'username': 'Cloud Apps Class'}
     return render_template('index.html', title='Home', user=user)
